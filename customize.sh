e358728392cc7b94defc780eaa639e2600a0b07b
Vishal Rathaur <cute.16v@gmail.com>
2026-02-02 11:32:07 +0530

    Add Miui/HyperOS Debloater Installer script
    This script installs the Miui/HyperOS Debloater Module, displaying system information and initializing cleanup.

---
+++ customize.sh
@@ -0,0 +1,78 @@
+#!/system/bin/sh
+# Miui Debloat by Vishal - Installer (v1225)
+logcat -G 128K &>/dev/null
+
+# Environment Detection
+if [ "$AXERON" == "true" ]; then
+    PRINT="printf"
+else
+    PRINT="ui_print"
+fi
+
+# Fixed Print Wrapper
+print_msg() {
+    case "$PRINT" in
+        ui_print) ui_print "$1" ;;
+        printf) printf "$1\n" ;;
+    esac
+}
+
+# BRANDING HEADER
+# Fixed ASCII art with double backslashes (\\)
+print_msg "*****************************************"
+print_msg "  Miui/HyperOS Debloater Module/Plugin | V4.04            "
+print_msg "  by Vishal Rathaur                      "
+print_msg "*****************************************"
+print_msg ""
+print_msg "  __      _______  _____ _    _          _      "
+print_msg "  \\ \\    / /_   _|/ ____| |  | |   /\\   | |     "
+print_msg "   \\ \\  / /  | | | (___ | |__| |  /  \\  | |     "
+print_msg "    \\ \\/ /   | |  \\___ \\|  __  | / /\\ \\ | |     "
+print_msg "     \\  /   _| |_ ____) | |  | |/ ____ \\| |____ "
+print_msg "      \\/   |_____|_____/|_|  |_/_/    \\_\\______|"
+print_msg ""
+
+# System Information
+ANDROIDVERSION=$(getprop ro.build.version.release)
+DEVICES=$(getprop ro.product.board)
+MANUFACTURER=$(getprop ro.product.manufacturer)
+NAME="Debloater | Vishu ❤️"
+VERSION="1225 Stable"
+DATE=$(date)
+
+print_msg "Hello Sir/Ma'am 👋"
+print_msg ""
+sleep 0.4
+print_msg " Let's Clean up the Trash that isn't necessary "
+sleep 0.2
+print_msg "Initializing cleanup for ${MANUFACTURER}..."
+print_msg ""
+print_msg "***************************************"
+print_msg ""
+print_msg "- Name            : ${NAME}"
+print_msg "- Version         : ${VERSION}"
+print_msg "- Android Version : ${ANDROIDVERSION:-Unknown}"
+print_msg "- Current Date    : ${DATE}"
+print_msg "***************************************"
+print_msg ""
+print_msg "- Device         : ${DEVICES:-Unknown}"
+print_msg "- Manufacturer    : ${MANUFACTURER:-Unknown}"
+print_msg "***************************************"
+print_msg ""
+print_msg "   - Miui/HyperOS Debloater Module/Plugin -    "
+print_msg ""
+print_msg "*****************************************"
+
+# --- INSTALLATION LOGIC ---
+print_msg "- Extracting module files..."
+# Unzip webroot specifically
+unzip -o "$ZIPFILE" 'webroot/*' -d "$MODPATH" >&2
+
+# PRO TIP: Ensure system folder for compliance
+mkdir -p "$MODPATH/system"
+
+print_msg "- Setting up permissions..."
+set_permalink_recursive "$MODPATH/webroot" 0 0 0755 0644
+
+print_msg "- Miui/HyperOS Debloater Module  is ready."
+print_msg "*****************************************"
